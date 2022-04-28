3dca5b837e110f0acb4abea68fa252669454d358
Chaim Lev-Ari <chiptus@gmail.com>
2022-04-28 09:46:41 +0300

    refactor(edge-devices): convert view to react
    refactor(edge): remove ref to edge devices module

--- app/edge/__module.js
+++ app/edge/__module.js
@@ -1,13 +1,14 @@
 import angular from 'angular';
 
 import edgeStackModule from './views/edge-stacks';
-import edgeDevicesModule from './devices';
 import { componentsModule } from './components';
 import { WaitingRoomViewAngular } from './EdgeDevices/WaitingRoomView';
+import { EdgeDevicesViewAngular } from './EdgeDevices/EdgeDevicesView';
 
 angular
-  .module('portainer.edge', [edgeStackModule, edgeDevicesModule, componentsModule])
+  .module('portainer.edge', [edgeStackModule, componentsModule])
   .component('waitingRoomView', WaitingRoomViewAngular)
+  .component('edgeDevicesView', EdgeDevicesViewAngular)
   .config(function config($stateRegistryProvider) {
     const edge = {
       name: 'edge',
@@ -112,7 +113,7 @@ angular
       },
     };
 
-    const edgeDevices = {
+    $stateRegistryProvider.register({
       name: 'edge.devices',
       url: '/devices',
       views: {
@@ -120,7 +121,7 @@ angular
           component: 'edgeDevicesView',
         },
       },
-    };
+    });
 
     if (process.env.PORTAINER_EDITION === 'BE') {
       $stateRegistryProvider.register({
@@ -147,6 +148,4 @@ angular
     $stateRegistryProvider.register(edgeJobs);
     $stateRegistryProvider.register(edgeJob);
     $stateRegistryProvider.register(edgeJobCreation);
-
-    $stateRegistryProvider.register(edgeDevices);
   });
